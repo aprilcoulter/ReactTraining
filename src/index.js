08e62e28619d3e021133b8af29b0589639ed3515
aprilcoulter <64415365+aprilcoulter@users.noreply.github.com>
2020-04-28 11:44:20 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,53 @@
+// // /*
+// // 🚨Game Overview🚨
+
+// // - The game is played in the browser
+// // - The user is asked their name via the `prompt()` function. Their name is stored for later use.
+// // - Questions and answers are stored in variables (only basic types are used).
+// // - The user's score is initially zero. If they get the the answer correct, the score increases.
+// // - The user submits their answers via the `prompt()` function.
+// // - At the end of each answer, the score will either increase or stay the same, and the next question will appear.
+// // - Once the user has answered all of the questions, an `alert()` function will trigger, letting them know their score given in the following format:
+
+// // - **0-2** "Oh wow {name}, you can do better."
+// // - **3-4** "Keep practicing {name}, you got this!"
+// // - **5** "Wow {name}, a perfect score!"
+// // */
+// // 
+// let score = 0;
+// let username;
+// // username = prompt('what is your name') 
+// console.log(username)
+
+// //🚨setup questions and answers🚨
+// let question1 = prompt('what class are you in')
+// let answer1 = 'react'
+// let question2 = prompt('what is your instructors name')
+// let answer2 = 'Michael'
+// let question3 = prompt('what is 2+2')
+// let answer3 = '4'
+// let question4 = prompt('What is your instructors age?')
+// let answer4 = '31'
+
+// let question5 = prompt('What application are you in?')
+// let answer5 = 'codesandbox'
+ 
+// if (question1 === answer1) {
+//   score = score + 1
+// } if (question2 === answer2) {
+//   score = score + 1
+// } if (question3 === answer3) {
+//   score = score + 1
+// } if (question4 === answer4) {
+//   score = score + 1
+// }
+// if (question5 === answer5) {
+//   score = score + 1
+// }
+
+// if(score <=3){
+//  alert("better luck next time");
+
+// }else{
+// alert('whoa! you did great!')
+// }
